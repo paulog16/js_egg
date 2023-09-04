1f39a462f69bc5751d8b0f0839a0aaae278c9c42
paulo guidolin <pguidolin0@gmail.com>
2023-09-04 00:17:09 -0300

    en proceso ejer 5

---
+++ ejercicio5/js.js
@@ -0,0 +1,10 @@
+function oper(){
+    var num1=document.getElementById('num1').value;
+    var num2=document.getElementById('num2').value;
+    var op=Document.getElementById('op').value;
+    if (op=='s'){
+        var result=num1+num2;
+        
+
+    }
+}
